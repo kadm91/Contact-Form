efeff10cc852657943c572ed429b01801d78a814
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2024-01-03 22:31:25 -0700

    Initial Commit

---
+++ Contact Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Kevin Martinez on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
